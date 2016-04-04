d1880ff825e7e688846fe4c66346765414a82fd1
wangshigen <979300544@qq.com>
2016-04-04 10:47:33 +0800

    添加mapper映射文件及mapper Java类

---
+++ src/com/future/hist/crm/dao/DepartmentMapper.java
@@ -0,0 +1,19 @@
+package com.future.hist.crm.dao;
+
+
+public interface DepartmentMapper {
+
+	/*public List<Department> getAllDp();
+	
+	public void deleteDepartment(Integer departmentId);
+	
+	public void updateDepartment(Department department);
+	
+	public Department findById(Integer departmentId);
+	
+	public Department findByName(String departmentName);
+	
+	public void addDepartment(Department department);*/
+	
+	//以上是以前写的，能用就用，不能用就直接删
+}
